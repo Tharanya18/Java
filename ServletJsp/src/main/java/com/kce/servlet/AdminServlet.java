be36f8b318375a030cb8305e5d54dc183b69d01e
Tharanya18 <stharanya43@gmail.com>
2026-07-08 14:17:17 +0530

    Add files via upload

---
+++ ServletJsp/src/main/java/com/kce/servlet/AdminServlet.java
@@ -0,0 +1,20 @@
+package com.kce.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AdminServlet")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html");
+		response.getWriter().print("<html>" + "<body> <b> WELCOME TO ADMIN PAGE" + " </b> </body> <html>");
+	}
+
+}
+				
